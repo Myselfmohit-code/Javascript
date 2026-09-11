f7008febc9fb48ea9bb0992dd9fe2f25d23e70f7
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-12 00:32:52 +0530

    Add JavaScript variable examples

--- Variable.js
+++ Variable.js
@@ -19,3 +19,43 @@
 //var z = x + y;
 
 //console.log(z);
+
+/* fullName = "mohit";
+age = 22;
+price = 99.99;
+radius = 5;
+a = null;
+y = undefined;
+isFollowing = true; 
+
+console.log(fullName);  
+console.log(age);
+console.log(price);
+console.log(radius);
+console.log(a);
+console.log(y);
+console.log(isFollowing);
+ */
+
+
+const Product={
+    title : "Ball pen",
+    price : 10,
+    inStock : true,
+    color : "blue"
+};
+console.log(Product);
+
+
+let x = 5;
+let y = null;
+let name = "mohit";
+let isstudent = true;
+let z = undefined;
+
+console.log(x);
+console.log(y);
+console.log(name);
+console.log(isstudent);
+console.log(z);
+
