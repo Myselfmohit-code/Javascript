1a7794f56ab1ae19cb80878759939d78f9f9a8b9
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-11 14:17:22 +0530

    Initial commit

---
+++ Variable.js
@@ -0,0 +1,21 @@
+//console.log("Hello, World!");
+
+//let x = 5;
+//let y = 6;
+//let z = x + y;
+
+//console.log(z);
+
+
+//const x = 5;
+//const y = 11;
+//const z = x + y;
+
+//console.log(z);
+
+
+//var x = 5;
+//var y = 16;
+//var z = x + y;
+
+//console.log(z);
