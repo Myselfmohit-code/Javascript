cf75dd76317b12d46f303eda3638fcad7debc7cf
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-13 00:01:44 +0530

    Add JavaScript operator examples

--- operator.js
+++ operator.js
@@ -1,5 +1,71 @@
-let x = 100 + 50;
-let y = 10 * 2;
-let z = x - y;
+//Arithmatic Operators
 
-console.log("The value of z is: " + z);
+/*let x = 5;
+let y = 2;
+let z = x + y; // Addition
+let a = x - y; // Subtraction
+let b = x * y; // Multiplication
+let c = x / y; // Division
+let d = x % y; // Modulus
+let e = x ** y; // Exponentiation
+
+
+console.log(z);
+console.log(a);
+console.log(b);     
+console.log(c);
+console.log(d);
+console.log(e);*/
+
+//Assignment Operators 
+
+/*let x = 10;
+x += 5; // x = x + 5
+x -= 3; // x = x - 3
+x *= 2; // x = x * 2
+x /= 2; // x = x / 2
+x %= 3; // x = x % 3
+x **= 2; // x = x ** 2
+
+console.log(x);*/
+
+//logical Operators
+
+/*let a = 5;
+let b = 10;
+console.log(a == b); // false
+console.log(a != b); // true
+console.log(a > b); // false
+console.log(a < b); // true
+console.log(a >= b); // false
+console.log(a <= b); // true
+console.log(a === b); // false
+console.log(a !== b); // true
+
+let x = true;
+let y = false;
+console.log(x && y); // false
+console.log(x || y); // true
+console.log(!x); // false*/
+
+//String Operators
+
+/*let str1 = "Hello";
+let str2 = "World";
+let str3 = str1 + " " + str2; // "Hello World"
+console.log(str3);
+*/
+
+//Type Operators
+
+/*let x = 5;
+console.log(typeof x); // "number"
+let date = new Date();
+console.log(date instanceof Date); // true
+*/
+
+
+//ternary Operator
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No"; // "Yes"
+console.log(canVote);
