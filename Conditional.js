993232216f379ec011bf606ed6ed2893cfb9f2f7
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-14 00:02:37 +0530

    Add conditional grading example

--- Conditional.js
+++ Conditional.js
@@ -4,10 +4,20 @@
   //  block of code to be executed if the condition is false
 }*/
 
-let age = 18;
+/*let age = 18;
 
 if (age >= 18) {
     console.log("You are eligible to vote");
 } else {
     console.log("You are not eligible to vote");
+}*/
+
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 60) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
 }
