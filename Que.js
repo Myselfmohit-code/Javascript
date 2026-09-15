e4b53cb087d13534ad6d2b327c17cf51450b9575
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-15 10:51:51 +0530

    Create Que.js

---
+++ Que.js
@@ -0,0 +1,39 @@
+//Que 1: take input from the user check number is multiple of 5 or not
+
+/*let number = prompt("Enter a number: ");
+
+if (number % 5 === 0) {
+    console.log(number + " is a multiple of 5");
+} else {
+    console.log(number + " is not a multiple of 5");
+}*/
+
+//Que 2: take input from the user check number is even or odd
+
+/*const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter your age: ", (age) => {
+    console.log("Your age is:", age);
+    rl.close();
+});*/
+
+//Que 3: give grade to the student based on marks
+
+/*let marks = prompt("enter your marks: ");
+if (marks >= 90) {
+    console.log("Grade A");         
+}else if (marks >= 60) {
+    console.log("Grade B");
+}else if (marks >= 40) {
+    console.log("Grade C");
+}else {
+    console.log("Grade D");
+}*/
+
+
+
