0654d3c0f0f4143fd6f3b54d4d51b6c1ffa7f0a0
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-15 10:53:56 +0530

    Add ticket price calculator

--- Que.js
+++ Que.js
@@ -35,5 +35,41 @@ if (marks >= 90) {
     console.log("Grade D");
 }*/
 
+export function getTicketPrice(age, isWeekend) {
+
+    if (typeof age !== 'number' || age < 0) {
+        throw new Error('Invalid age');
+    }
+
+    if (typeof isWeekend !== 'boolean') {
+        throw new Error('Invalid isWeekend value');
+    }
+
+    let price;
+
+    if (age < 13) {
+        price = 8;
+    } 
+
+    else if (age >= 13 && age <= 17) {
+        price = 12;
+    } 
+
+    else if (age >= 18 && age <= 59) {
+        price = 15;
+    } 
+
+    else {
+        price = 10;
+    
+    }
+
+    
+    if (isWeekend) {
+        price += 5;
+    }
+
+    return price;
+}
 
 
