c0309eff5bdeff6de3bb504e3ae2fc1d4a47111f
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-24 00:04:44 +0530

    Add loop and guessing number exercises

---
+++ loopsQue.js
@@ -0,0 +1,22 @@
+//Que 1 print 1 to 100 even number
+/*
+for (let i =  0; i <=100; i++){
+    if(i % 2 ===0){
+        console.log(i);
+        }
+}*/     
+
+//gussing number 
+/*
+let gameNum = 25;
+
+let userNum = prompt("enter the game number");
+while (userNum != gameNum){
+   userNum = prompt("guess again")
+
+}console.log("congrat,you entered write number")*/
+
+
+
+
+
