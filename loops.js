ad1ad514950644701403303e0023790fa6f2dfa6
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-23 23:44:58 +0530

    Add loop examples

---
+++ loops.js
@@ -0,0 +1,59 @@
+// for loops 
+/*for (let i =1; i<=5;i++){
+
+    console.log("mohit")
+}*/
+
+//calculate sum 1 to n 
+
+/*let sum = 0;
+let n = 100;
+for (let i = 1; i<=n;i++){
+    sum = sum + i;
+    console.log("sum =",sum)
+}*/
+
+
+//while loops 
+
+/*while (condition){
+
+}*/
+
+/*let i = 1;
+while( i <=5){
+    console.log("n");
+    i++
+}*/
+
+//do while 
+
+/*let i = 20;
+do{
+    console.log("mohit");
+    i++;
+}while(i <=10);*/
+
+
+//for-of loops 
+
+/*let str = "Mohit";
+let size = 0;
+for (let i of str){
+    console.log("i =",i);
+    size++;
+}console.log(size);*/
+
+
+// for-in loop
+
+/*let student = {
+    name : "mohit kr singh",
+    age : 21,
+    cgpa : 8.0,
+    ispass : true
+};
+for(let key in student){
+    console.log("key = ",key,"value =",student[key]);
+} */
+
