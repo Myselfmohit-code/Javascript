5f890690fe5a389e3d1956021d49b4feb3229cd2
Mohit Kumar Singh <immohitsingh1327@gmail.com>
2026-09-25 00:17:57 +0530

    Initial commit

---
+++ Dom.js
@@ -0,0 +1,16 @@
+//let heading = document.getElementById("heading");
+//console.log(heading);
+
+//let main = document.getElementsByClassName("main");
+//console.log(main);
+//console.dir(main);
+
+//let main = document.getElementsByTagName("h1");
+//console.dir(main);
+//console.log(main);
+
+//let firstEl = document.querySelector("p");  //1st element
+//console.dir(firstEl);
+
+//let allEl = document.querySelectorAll("p");  //all element
+//console.dir(allEl); 
